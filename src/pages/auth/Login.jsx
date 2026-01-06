888831ff68af7d53dc4f2caba15de7670b554aa4
Irtiza Ali <aliirtiza859@gmail.com>
2026-01-07 00:22:18 +0500

    css update

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 import MyLogo from "../../assets/icons/logo.png";
-import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 import api from "../../api/axios";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
